1df87162d4cd95435ff04e059ede5accafb9b384
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2024-09-09 02:34:05 +0700

    Update pre-data.sh

--- exe/pre-data.sh
+++ exe/pre-data.sh
@@ -1,4 +1,4 @@
-# clone model sentence-transformers, tiến hành truy xuất
+huggingface-cli login --token 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
 cd ..
 git clone https://github.com/UKPLab/sentence-transformers.git
 cd sentence-transformers
@@ -9,4 +9,5 @@ mkdir data
 cd data
 gdown 1zCjH1YsGgEVgegTl-RVOMwaEy_7p3Uht
 python -m zipfile -e Dataset-Fake-Real-News-Gathering.zip .
+cd ..
 python src/data_transform.py
