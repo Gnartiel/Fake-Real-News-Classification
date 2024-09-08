d87d5ca4e696576c645cca509146896da51dc79e
Gnartiel <105764822+Gnartiel@users.noreply.github.com>
2024-09-08 14:11:46 +0700

    Update train-sentence-BERT.py

--- src/train-sentence-BERT.py
+++ src/train-sentence-BERT.py
@@ -54,35 +54,18 @@ def main():
             train_examples.append(InputExample(texts=[example[0], example[1]], label=int(example[2])))
     # model = SentenceTransformer("multi-qa-mpnet-base-dot-v1")
     model = SentenceTransformer("Gnartiel/vietnamese-sbert", use_auth_token='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')
-    train_loss = losses.ContrastiveLoss(model=model)
+    train_loss = losses.TripletLoss(model=model)
     train_dataset = SentencesDataset(train_examples, model)
     train_dataloader = DataLoader(train_dataset, batch_size=32)
     num_epochs = 2
     warmup_steps = int(len(train_dataloader) * num_epochs * 0.1)
     model_save_path = os.path.join(path_root, 'model')
-    # checkpoint_path  = os.path.join(path_root, 'model/checkpoint')
-    # checkpoint_save_steps  = 200
-    # checkpoint_save_total_limit = 3
     model.fit(
         train_objectives=[(train_dataloader, train_loss)], 
         epochs=num_epochs,
         output_path=model_save_path,
         warmup_steps=warmup_steps,
-        # checkpoint_path  = checkpoint_path,
-        # checkpoint_save_steps = checkpoint_save_steps,
-        # checkpoint_save_total_limit = checkpoint_save_total_limit, 
         callback= ClearMemory())
-
-    # # model.push_to_hub('presencesw/DSC-Believer-SBERT')
-    # model.save_to_hub(
-    #     repo_name= "presencesw/DSC-Believer-SBERT_v2",
-    #     exist_ok=True,
-    # )
-
-    # model.save_to_hub(
-    #     repo_name= "Gnartiel/vietnamese-sbert",
-    #     exist_ok=True,
-    # )
     model.save_pretrained("./Fake-Real-News-Classification", from_pt=True) 
 if __name__ == "__main__":
     main()
